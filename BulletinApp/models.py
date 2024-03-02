c1be1662f510dbab3b42b1921fb7799200dbbb95
Jonah Walker <51419221+Jwrecker@users.noreply.github.com>
2024-03-01 22:37:17 -0700

    Update models.py

--- BulletinApp/models.py
+++ BulletinApp/models.py
@@ -27,6 +27,7 @@ class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
     bulletin = models.ForeignKey(Bulletin, on_delete=models.CASCADE)
+    post_picture = models.ImageField(upload_to='post_pictures/')
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     pub_date = models.DateTimeField()
