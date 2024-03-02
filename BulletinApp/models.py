b48181b5055c87ecd53d14f62d8e8d94e64097a5
Jonah Walker <51419221+Jwrecker@users.noreply.github.com>
2024-03-01 22:34:13 -0700

    Update models.py

--- BulletinApp/models.py
+++ BulletinApp/models.py
@@ -10,7 +10,8 @@ class Bulletin(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    location = models.CharField(max_length=200)
+    latitude = models.FloatField(db_index=True)
+    longitude = models.FloatField(db_index=True)
 
     def __str__(self):
         return self.title
