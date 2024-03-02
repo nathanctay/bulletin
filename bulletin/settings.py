d3a6195968b91ee96ff379f1f9ead16566befd2a
nathan <nathanctay@gmail.com>
2024-03-01 20:29:47 -0700

    installed app

--- bulletin/settings.py
+++ bulletin/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'BulletinApp.apps.BulletinappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
